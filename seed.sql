4826aeec55b0178534d64034caeab7a76b567c84
kevinjwesley <kevin.john.wesley@gmail.com>
2021-05-25 08:07:30 -0600

    update packag.json

--- seed.sql
+++ seed.sql
@@ -5,13 +5,13 @@ CREATE DATABASE tracker_db;
 USE tracker_db;
 
 CREATE TABLE department (
-  id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  id INT PRIMARY KEY AUTO_INCREMENT DEFAULT "0",
   name VARCHAR(30) 
   
 );
 
 CREATE TABLE role (
-id INT PRIMARY KEY NOT NULL,
+id INT PRIMARY KEY,
 title VARCHAR(30),
 salary DECIMAL(10,4),
 department_id INT NOT NULL
@@ -19,7 +19,7 @@ department_id INT NOT NULL
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY NOT NULL,
+    id INT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT NOT NULL,
